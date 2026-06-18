2476f00bdd147262a14ba15334ced5c3baf1cfcb
Yuchan Lee <lasuillard@gmail.com>
2026-06-18 15:01:03 +0000

    Skip downloading unnecessary tools when running CI commands via `nix`

--- flake.nix
+++ flake.nix
@@ -16,15 +16,12 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages = {
+          inherit (pkgs) pre-commit just shfmt shellcheck mockoon nginx;
+        };
+
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            pre-commit
-            just
-            shfmt
-            shellcheck
-            mockoon
-            nginx
-          ];
+          packages = builtins.attrValues self.packages.${system};
           shellHook = ''
             pre-commit install
           '';
